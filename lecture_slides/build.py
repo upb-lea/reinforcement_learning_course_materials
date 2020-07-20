d33a793bc343727b3d502f7a27bd0f2999a96e7b
wkirgsn <wilkir@gmx.de>
2020-07-20 16:56:16 +0200

    add build.py for rendering PDFs from lecture_notes tex files

--- lecture_slides/build.py
+++ lecture_slides/build.py
@@ -7,7 +7,17 @@ os.makedirs('built', exist_ok=True)
 call_pdflatex_l = ['pdflatex', '-synctex=1',
                    '-interaction=nonstopmode', 'main.tex']
 
+
+def clear_tex_binaries():
+    # clean misc files
+    for file in os.listdir('.'):
+        if file.startswith('main'):
+            if not file.endswith(('.tex', '.pdf')):
+                os.remove(file)
+
+
 # build main pdf
+clear_tex_binaries()
 with fileinput.input('main.tex', inplace=True) as f:
     for line in f:
         if 'includeonly{' in line:
@@ -16,12 +26,15 @@ with fileinput.input('main.tex', inplace=True) as f:
         else:
             print(line, end='')
 call(call_pdflatex_l)
+call(call_pdflatex_l)
 os.replace('main.pdf', os.path.join('built', 'main.pdf'))
 
 # build single chapters
 chap_list = [os.path.splitext(p)[0] for p in os.listdir('tex')
              if p.endswith('.tex')]
+
 for tex_file in tqdm(chap_list):
+    clear_tex_binaries()
     with fileinput.input('main.tex', inplace=True) as f:
         for line in f:
             if 'includeonly{' in line:
@@ -30,4 +43,10 @@ for tex_file in tqdm(chap_list):
             else:
                 print(line, end='')
     call(call_pdflatex_l)
+    call(call_pdflatex_l)
+    call(call_pdflatex_l)
     os.replace('main.pdf', os.path.join('built', tex_file + '.pdf'))
+
+print('Done processing:')
+for p in chap_list:
+    print('  ', p)
