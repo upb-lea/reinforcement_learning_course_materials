3b7f940dc7f91a9228c46129801a0742a859f60d
wkirgsn <wilkir@gmx.de>
2020-07-27 16:47:41 +0200

    add travis command references for auto building tex files and submitting to public upb webdomain

---
+++ texlive/texlive_install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+# Originally from https://github.com/latex3/latex3
+# Copied from https://tex.stackexchange.com/questions/398830/how-to-build-my-latex-automatically-using-travis-ci
+
+# This script is used for testing using Travis
+# It is intended to work on their VM set up: Ubuntu 12.04 LTS
+# A minimal current TL is installed adding only the packages that are
+# required
+
+# See if there is a cached version of TL available
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+if ! command -v texlua > /dev/null; then
+  # Obtain TeX Live
+  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+  tar -xzf install-tl-unx.tar.gz
+  cd install-tl-20*
+
+  # Install a minimal system
+  ./install-tl --profile=../texlive/texlive.profile
+
+  cd ..
+fi
+
+# Just including texlua so the cache check above works
+# Needed for any use of texlua even if not testing LuaTeX
+tlmgr install luatex
+
+# Other contrib packages: done as a block to avoid multiple calls to tlmgr
+# texlive-latex-base is needed to run pdflatex
+tlmgr install   \
+  exam          \
+  amsfonts      \
+  stmaryrd      \
+  amsmath
+
+# Keep no backups (not required, simply makes cache bigger)
+tlmgr option -- autobackup 0
+
+# Update the TL install but add nothing new
+tlmgr update --self --all --no-auto-install
