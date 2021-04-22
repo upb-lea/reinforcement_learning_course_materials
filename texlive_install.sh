e66b071978e6e77c75ff02cd094f90ba31fc7750
Wilhelm Kirchgässner <16706935+wkirgsn@users.noreply.github.com>
2021-04-22 11:37:43 +0200

    temporarily reinstall texlive to overcome upgrade hurdle

--- texlive_install.sh
+++ texlive_install.sh
@@ -9,18 +9,19 @@
 # required
 
 # See if there is a cached version of TL available
-export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
-if ! command -v texlua > /dev/null; then
-  # Obtain TeX Live
-  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-  tar -xzf install-tl-unx.tar.gz
-  cd install-tl-20*
+# 22.04.21, WK: TEMPORARILY REINSTALL DUE TO https://tug.org/texlive/upgrade.html
+#export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+#if ! command -v texlua > /dev/null; then
+# Obtain TeX Live
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar -xzf install-tl-unx.tar.gz
+cd install-tl-20*
 
-  # Install a minimal system
-  ./install-tl --profile=../texlive/texlive.profile
+# Install a minimal system
+./install-tl --profile=../texlive/texlive.profile
 
-  cd ..
-fi
+cd ..
+#fi
 
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
